9f0af211bec086dc533680664339d0ba3afc2314
Choon-Chern Lim <limc@mayo.edu>
2017-05-23 15:39:42 -0500

    Initial commit

---
+++ src/main/groovy/com/github/choonchernlim/service/HelloWorldService.groovy
@@ -0,0 +1,10 @@
+package com.github.choonchernlim.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class HelloWorldService {
+    String getMessage() {
+        return 'Hello World'
+    }
+}
