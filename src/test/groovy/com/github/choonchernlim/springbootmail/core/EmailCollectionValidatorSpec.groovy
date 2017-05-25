c82b800b3aaf2cc44eacc946d6e41a706f4c7d02
Choon-Chern Lim <meteoric@rocketmail.com>
2017-05-25 17:11:02 -0500

    specs

---
+++ src/test/groovy/com/github/choonchernlim/springbootmail/core/EmailCollectionValidatorSpec.groovy
@@ -0,0 +1,30 @@
+package com.github.choonchernlim.springbootmail.core
+
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl
+import org.hibernate.validator.internal.engine.path.PathImpl
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class EmailCollectionValidatorSpec extends Specification {
+
+    def context = new ConstraintValidatorContextImpl(null, null, PathImpl.createRootPath(), null)
+    def validator = new EmailCollectionValidator()
+
+    @Unroll
+    def "isValid - given #label, should be #expectedBool"() {
+        when:
+        def actualBool = validator.isValid(collection, context)
+
+        then:
+        actualBool == expectedBool
+
+        where:
+        label                 | collection             | expectedBool
+        'null value'          | null                   | true
+        'empty value'         | []                     | true
+        'one valid email'     | ['a@a.com']            | true
+        'all valid emails'    | ['a@a.com', 'b@b.com'] | true
+        'one invalid email'   | ['aaaa']               | false
+        'some invalid emails' | ['aaaa', 'a@a.com']    | false
+    }
+}
